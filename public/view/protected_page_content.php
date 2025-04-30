f6929db1392c8e6d62a14460ca67bac439dab76f
ckaraduman <cemilkerkaraduman@gmail.com>
2025-04-30 15:12:00 +0300

    Test-2

--- public/view/protected_page_content.php
+++ public/view/protected_page_content.php
@@ -2,12 +2,10 @@
 <br>
 <h5>Eğlenceli vakit geçirirken donanım kazanacağınız sayfalarımıza hoş geldiniz...</h5>
 
+<h3>Mayıs 2025 itibariyle hediyeli bilgi yarışmamızın hazırlıklarına başladık!</h3>
+
 <div class="row mt-5">
-        <div class="col-6">
-            <!--<img src="/public/images/palmet_intranet.png" alt="Palmet_Intranet" width="600">-->
-        </div>
+    <div class="col-6">
+        <!--<img src="/public/images/palmet_intranet.png" alt="Palmet_Intranet" width="600">-->
     </div>
-
-
-
-
+</div>
