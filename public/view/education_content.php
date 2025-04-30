7b808110d8a905601f83ee3da95140f1677b556b
ckaraduman <cemilkerkaraduman@gmail.com>
2025-04-30 17:47:11 +0300

    Test-2

--- public/view/education_content.php
+++ public/view/education_content.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
     <h1>Eğitimler</h1>
     <div class="row">
         <div class="col-6">
             <!--<img src="/public/images/education.png" alt="Education" width="800">-->
+            <h3>Python Eğitimleri</h3>
+            <h3>Excel Visual Basic ve Makro Eğitimleri</h3>
         </div>
-    </div>   
+    </div>
 </body>
+
 </html>
