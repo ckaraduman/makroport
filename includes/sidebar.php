c5ea1bbecc4186bea163a736034577bf5e2396bc
Cem İlker Karaduman <cemilkerkaraduman@gmail.com>
2025-07-12 14:30:15 +0300

    Menu-Correction-2

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
     <div class="list-group">
         <a href="/" class="list-group-item list-group-item-action">Ana Sayfa</a>
         <a href="/about" class="list-group-item list-group-item-action">Hakkımızda</a>
-        <a href="/software" class="list-group-item list-group-item-action">MakroPort Yazılım</a>
+        <a href="/software" class="list-group-item list-group-item-action">Yazılım Çözümleri</a>
         <a href="/course" class="list-group-item list-group-item-action">Eğitimler</a>
         <a href="/blog" class="list-group-item list-group-item-action">Yazılar</a>
         <!-- <a href="/list-quotes" class="list-group-item list-group-item-action">Alıntılar-Yorumlar</a> -->
