93cfbc7cdc5944394f48086a80f63c43c6672ffe
Cem İlker Karaduman <cemilkerkaraduman@gmail.com>
2025-07-15 13:33:38 +0300

    Building-4

--- pages/makrodocs.php
+++ pages/makrodocs.php
@@ -7,19 +7,28 @@ Fiziksel evrak yığınlarından kurtulun. Evrakları dijital olarak arşivleyin
 Ayrıca tüm belge süreçlerinde hiyerarşik onay mekanizması ile kontrol ve denetim sağlarsınız.";
 
 ?>
+
 <div class="container">
-<h2>MakroPort – Yeni Çözüm Ortağınız</h2>
-<hr>
-<?php if (isset($_SESSION['user_id'])): ?>
-<p>Hoş geldiniz, <?= htmlspecialchars($_SESSION['fullname']) ?></p>
-    <button type="button" class="btn btn-primary">MakroDocs</button>
-<?php else: ?>
-    <div class="row align-items-start">
-        <div class="col-md-6">
-            <p style="text-align: justify;"><?php echo nl2br($message1); ?></p>
-        <div class="col-md-6">
-            <img src="assets/img/MakroDocs.png" alt="Takım Resmi" class="img-fluid rounded shadow" style="max-width: 100%; height: auto;">
+    <h2>MakroDocs – Dijital Doküman Yönetim Sistemi</h2>
+    <hr>
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <p>Hoş geldiniz, <?= htmlspecialchars($_SESSION['fullname']) ?></p>
+        <button type="button" class="btn btn-primary">MakroDocs</button>
+    <?php else: ?>
+        <div class="row align-items-start">
+            <!-- Sol taraf: Metin -->
+            <div class="col-md-6">
+                <img src="assets/img/MakroDocs.png" alt="Takım Resmi" class="img-fluid rounded shadow mb-3" style="max-width: 70%; height: auto;">
+            </div>
+
+            <!-- Sağ taraf: Resim ve butonlar alt alta -->
+            <div class="col-md-6 text-center">
+                <p style="text-align: justify;"><?php echo nl2br($message1); ?></p>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <a class="btn btn-primary mb-2" href="/login" role="button">Giriş Yap</a>
+                    <a class="btn btn-secondary" href="/register" role="button">Kayıt Ol</a>
+                </div>
+            </div>
         </div>
-    </div>
-<?php endif; ?>
+    <?php endif; ?>
 </div>
